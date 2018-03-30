3a3f71e55796374320ef9ffec2c6571ce9118d33
Lorena <lorena.giddens@outlook.com>
2018-03-30 03:32:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = { [bjork]: "lionsong", [solvent]: "loss for words" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist.slowdive = ""
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
