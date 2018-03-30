582f9060e878bd17a4a25b928b93cdf6b86b4052
Lorena <lorena.giddens@outlook.com>
2018-03-30 03:49:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,1 @@
-var playlist = new Object({ [Slowdive]: "Alison", [My Bloody Valentine]: "Sometimes" });
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['Phil Ochs'] = "Here's to the State of Mississippi"";
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  
-}
+var playlist = { Slowdive: "Alison"};
