f62e12e202ee4ceb0695982b4795643727282b9d
Lorena <lorena.giddens@outlook.com>
2018-03-30 03:52:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['My Bloody Valentine'] = "Sometimes";
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  
+}
